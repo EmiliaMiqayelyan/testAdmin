a7dc612821f0689e104f4ceb275f28ea42573ce4
Emilia <emilik200915@gmail.com>
2021-10-12 01:46:25 +0400

    add files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './app/App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Home from "./Home";
+import Login from "./Login";
+import Panel from "./Panel";
+
+export {Home, Login,Panel}
 
 ReactDOM.render(
   <React.StrictMode>
