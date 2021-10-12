4e1b69f11ba07382981c7be20dc841c5445aeb98
EmiliaMiqayelyan <88584270+EmiliaMiqayelyan@users.noreply.github.com>
2021-10-12 18:45:52 +0400

    Delete src/Home.js directory

--- src/Home.js/Index.js
+++
@@ -1,8 +0,0 @@
-import { Button } from "react-bootstrap"
-import { Link } from "react-router-dom";
-export default ()=>{
-    return <div>
-        Thie is Home
-        <Link to = '/login'><Button>Login</Button></Link>
-    </div>
-}
