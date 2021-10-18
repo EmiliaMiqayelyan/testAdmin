8c92afc5515b94f325df43e16611b0ec97f926b1
EmiliaMiqayelyan <88584270+EmiliaMiqayelyan@users.noreply.github.com>
2021-10-18 23:56:30 +0400

    Update doctorsList.js

--- src/views/Doctors/doctorsList.js
+++ src/views/Doctors/doctorsList.js
@@ -1,30 +1,41 @@
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 import { Table } from "react-bootstrap";
-import { Link } from "react-router-dom";
-import hook from "./hook";
 
-export default ()=>{
-    const {doctors,match} = hook();
-    return <div>
 
-<Table striped bordered hover variant="dark">
-  <thead>
-    <tr>
-      <th>#</th>
-      <th>First Name</th>
-      <th>Last Name</th>
-      <th>Tools</th>
-    </tr>
-  </thead>
-  <tbody>
-      {doctors?doctors.map(doctor=>{
-          return <tr>
+export default function DoctorList(){
+  const [user, setUser] = useState({
+    id:"",
+    firstName: "",
+    lastnName: ""
+  });
+
+  const { id } = useParams();
+  useEffect(() => {
+    loadUser();
+  }, []);
+  const loadUser = async () => {
+        const responce = await axios.get("users/get-doctors/?with_slots=False")
+    setUser(responce.data);
+  };
+
+  return (
+    <Table striped bordered hover variant="dark">
+    <thead>
+      <tr>
+        <th>#</th>
+        <th>First Name</th>
+        <th>Last Name</th>
+      </tr>
+    </thead>
+    <tbody>
+        <tr>
           <td>{doctor.id}</td>
-          <td>{doctor.first_name}</td>
-          <td>{doctor.last_name}</td>
-          <td><Link to={`${match.url}/${doctor.id}`} >Edit</Link></td>
-        </tr>
-      }):"Loading..."}
-  </tbody>
+          <td>{doctor.firstName}</td>
+          <td>{doctor.lastName}</td>
+          </tr>
+          </tbody>
 </Table>
-    </div>
-};
+) };
+
