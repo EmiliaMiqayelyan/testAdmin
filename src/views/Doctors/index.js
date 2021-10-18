176bfb0965ab5e1208009eafb8eb7a2dc54d24b4
EmiliaMiqayelyan <88584270+EmiliaMiqayelyan@users.noreply.github.com>
2021-10-18 23:57:11 +0400

    Update index.js

--- src/views/Doctors/index.js
+++ src/views/Doctors/index.js
@@ -2,19 +2,10 @@ import { Table } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import hook from "./hook";
 
-export default () => {
-  const { doctors, match } = hook();
-  return <div>
+export default ()=>{
+    const {doctors,match} = hook();
+    return <div>
 
-<<<<<<< HEAD
-    <Table striped bordered hover variant="dark">
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>Tools</th>
-=======
 <Table striped bordered hover variant="dark">
   <thead>
     <tr>
@@ -25,25 +16,15 @@ export default () => {
     </tr>
   </thead>
   <tbody>
-      {doctors?doctors.map((doctor,index)=>{
-          return <tr key = {index}>
+      {doctors?doctors.map(doctor=>{
+          return <tr>
           <td>{doctor.id}</td>
           <td>{doctor.first_name}</td>
           <td>{doctor.last_name}</td>
           <td><Link to={`${match.url}/${doctor.id}`} >Edit</Link></td>
->>>>>>> 5e3231c0eaaac64929833544472754a98d95f7e3
         </tr>
-      </thead>
-      <tbody>
-        {doctors ? doctors.map(doctor => {
-          return <tr>
-            <td>{doctor.id}</td>
-            <td>{doctor.first_name}</td>
-            <td>{doctor.last_name}</td>
-            <td><Link to={`${match.url}/${doctor.id}`} >Edit</Link></td>
-          </tr>
-        }) : "Loading..."}
-      </tbody>
-    </Table>
-  </div>
+      }):"Loading..."}
+  </tbody>
+</Table>
+    </div>
 };
