16e1a60abf867b614a97c3d362e7f2d0fcb55d10
Emilia <emilik200915@gmail.com>
2021-10-24 20:49:38 +0400

    add

--- src/views/DoctorList/index.js
+++ src/views/DoctorList/index.js
@@ -3,7 +3,7 @@ import { useParams } from "react-router-dom";
 import axios from "axios";
 import { Table } from "react-bootstrap";
 
-export default DoctorList =()=>{
+export default DoctorList = () => {
   const [user, setUser] = useState({
     id: "",
     firstName: "",
